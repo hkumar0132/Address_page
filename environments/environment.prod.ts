d80756ae11a52271d4bab48dbb67cafd2053b0a6
hkumar0132 <hkumar0132@gmail.com>
2020-06-14 11:43:30 +0530

    commit

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase : {
+    apiKey: "", //Removed for security reasons
+    authDomain: "addresspage-c1da6.firebaseapp.com",
+    databaseURL: "https://addresspage-c1da6.firebaseio.com",
+    projectId: "addresspage-c1da6",
+    storageBucket: "addresspage-c1da6.appspot.com",
+    messagingSenderId: "540282354290",
+    appId: "1:540282354290:web:32d1d503d97c71fb9aa0eb",
+    measurementId: "G-T9SCVSY7LM"
+  },
   production: true
 };
--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  production: true,
   firebase : {
     apiKey: "", //Removed for security reasons
     authDomain: "addresspage-c1da6.firebaseapp.com",
@@ -9,5 +8,6 @@ export const environment = {
     messagingSenderId: "540282354290",
     appId: "1:540282354290:web:32d1d503d97c71fb9aa0eb",
     measurementId: "G-T9SCVSY7LM"
-  }
+  },
+  production: true
 };
